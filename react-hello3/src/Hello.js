d1ee39a397a5d9e0dacbc3a3b3087502d07dd2d5
bigheada35 <bigheada@naver.com>
2022-01-22 16:23:02 +0900

    2022-01-20

---
+++ react-hello3/src/Hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello(){
+
+    return (<h1>HelloWorld!!</h1>);
+}
+
+export default Hello;
---
+++ react-helloworld/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <p>Hell</p>
+      <p>Hell</p>
+      <p>Hell</p> */}
+
+      <Hello></Hello>
+      <Hello></Hello>
+      <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
